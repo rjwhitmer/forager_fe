6f15d79046fc9aaf39d7b7c68fda225946b93524
Bob Whitmer <rjwhitmercc@gmail.com>
2020-08-12 09:54:51 -0600

    buildpack

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Login(props){
     return (
         <form className='login'>
-            <p>This is the login form</p>
+            <p>This is the login</p>
             <input type='text' placeholder='Username' name='username' />
             <input type='password' placeholder='Password' name='password' />
             <button type='submit' onClick={props.handleLogin}>Log-In!</button>
